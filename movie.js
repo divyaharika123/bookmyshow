4fb6a7058e149d2b734d9272844ec6823623c5db
N.DivyaHarika <134125019+divyaharika123@users.noreply.github.com>
2023-07-26 15:20:22 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,91 @@
+var txt=document.getElementById("txt");
+var price=document.getElementById("price");
+var add=document.getElementById("add");
+// count=0;
+// add.addEventListener("click",()=>
+// {
+// if(count >= 0)
+// {
+//     count=count+1;
+//     txt.innerHTML=count;
+//     price.innerHTML=count*300;
+// }
+// });
+
+
+var royaltick=0;
+var royalprice=0
+add.addEventListener("click",()=>
+{
+    if(royaltick>=0){
+        royalprice=royalprice+500;
+        royaltick=royaltick+1;
+        txt.innerHTML=royaltick;
+        price.innerHTML=royalprice;    
+    }
+})     
+
+var royaltick=0;
+var royalprice=0
+add1.addEventListener("click",()=>
+{
+    if(royaltick>=0){
+        royalprice=royalprice+250;
+        royaltick=royaltick+1;
+        txt.innerHTML=royaltick;
+        price.innerHTML=royalprice;   
+        
+    }
+})
+
+var royaltick=0;
+var royalprice=0
+add2.addEventListener("click",()=>
+{
+    if(royaltick>=0){
+        royalprice=royalprice+250;
+        royaltick=royaltick+1;
+        txt.innerHTML=royaltick;
+        price.innerHTML=royalprice;   
+        
+    }
+})
+
+var royaltick=0;
+var royalprice=0
+add3.addEventListener("click",()=>
+{
+    if(royaltick>=0){
+        royalprice=royalprice+250;
+        royaltick=royaltick+1;
+        txt.innerHTML=royaltick;
+        price.innerHTML=royalprice;   
+        
+    }
+})
+
+var royaltick=0;
+var royalprice=0
+add4.addEventListener("click",()=>
+{
+    if(royaltick>=0){
+        royalprice=royalprice+250;
+        royaltick=royaltick+1;
+        txt.innerHTML=royaltick;
+        price.innerHTML=royalprice;   
+        
+    }
+})
+
+var royaltick=0;
+var royalprice=0
+add5.addEventListener("click",()=>
+{
+    if(royaltick>=0){
+        royalprice=royalprice+250;
+        royaltick=royaltick+1;
+        txt.innerHTML=royaltick;
+        price.innerHTML=royalprice;   
+        
+    }
+})
